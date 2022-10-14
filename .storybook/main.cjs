f21cd433e41951ee7b004d87f1c7f81b1a4d5437
Levy Mateus Macedo <levymateusmacedo@gmail.com>
2022-10-14 10:06:48 -0300

    ci: add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'production') {
+      config.base = '/lab-ds/'
+    }
+    return config
   }
 }
